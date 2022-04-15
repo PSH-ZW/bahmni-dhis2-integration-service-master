4c27cf7944df3ac3f2120f2abcf58176598d31fd
Vishnu <vishnu@nuchange.com>
2022-04-15 12:50:54 +0530

    remove code to bypass auth checking

--- src/main/java/com/thoughtworks/bahmnidhis2integrationservice/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/thoughtworks/bahmnidhis2integrationservice/interceptor/AuthenticationInterceptor.java
@@ -32,10 +32,6 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        TODO: bypassing preHandle check to run in local, remove this for production build.
-        if(true) {
-            return true;
-        }
         if (handler == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND,
                     "Application cannot handle url " + request.getRequestURI());
